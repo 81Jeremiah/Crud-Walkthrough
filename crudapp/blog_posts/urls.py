afed4a9132b408c3beffeed965ff8210930c20a9
81Jeremiah <jeremiahlutes@gmail.com>
2019-07-05 23:26:16 -0700

    corrected url patterns

--- crudapp/blog_posts/urls.py
+++ crudapp/blog_posts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = 'blog_posts'
-url_patterns = [
+
+urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('new/', views.post_create, name='post_new'),
     path('edit/<int:pk>/', views.post_update, name='post_edit'),
